e9e4e8a23176c663b2dc5460a837e2e12260aaee
EnGentech <engen.inyang@gmail.com>
2023-07-06 17:41:50 -0700

    archive created

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""this scrip is written with shutil to compress files"""
+
+from fabric.api import local
+from datetime import datetime
+
+current_time = datetime.now()
+new_time = current_time.strftime('%Y%m%d%H%M%S')
+
+def do_pack():
+    """define a function to pack all the files into an archive"""
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_\
+        {}.tgz web_static".format(new_time))
+
+        return "versions/web_static_{}".format(new_time)
+    except Exception as e:
+        return None
+
+# Coded by EnGentech
