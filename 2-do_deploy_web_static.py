1a480081476b3fae16dc8ce7fdda9064205938b1
EnGentech <engen.inyang@gmail.com>
2023-07-07 16:00:51 -0700

    modified

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,26 +11,31 @@ env.user = 'ubuntu'
 
 def do_deploy(archive_path):
     """Function for web deployment"""
-    if path.exists(archive_path):
-        tim = archive_path[20:-4]
-        put(archive_path, '/tmp/')
+    try:
+        if path.exists(archive_path):
+            tim = archive_path[20:-4]
+            put(archive_path, '/tmp/')
 
-        pad = "/data/web_static/releases/web_static_"
+            pad = "/data/web_static/releases/web_static_"
 
-        a = "sudo mkdir -p {}{}".format(pad, tim)
-        b = "sudo tar -xzf /tmp/web_static_\
-        {}.tgz -C {}{}".format(tim, pad, tim)
-        c = "sudo rm /tmp/web_static_{}.tgz".format(tim)
-        d = "sudo mv {}{}/web_static/* {}{}".format(pad, tim, pad, tim)
-        e = "sudo rm -rf {}{}/web_static".format(pad, tim)
-        f = "sudo rm -rf /data/web_static/current"
-        g = "sudo ln -s {}{}/data/web_static/current".format(pad, tim)
+            a = "sudo mkdir -p {}{}".format(pad, tim)
+            b = "sudo tar -xzf /tmp/web_static_\
+            {}.tgz -C {}{}".format(tim, pad, tim)
+            c = "sudo rm /tmp/web_static_{}.tgz".format(tim)
+            d = "sudo mv {}{}/web_static/* {}{}".format(pad, tim, pad, tim)
+            e = "sudo rm -rf {}{}/web_static".format(pad, tim)
+            f = "sudo rm -rf /data/web_static/current"
+            g = "sudo ln -s {}{}/data/web_static/current".format(pad, tim)
 
-        commands = [a, b, c, d, e, f, g]
-        for command in commands:
-            run(command)
+            commands = [a, b, c, d, e, f, g]
+            for command in commands:
+                run(command)
 
-    else:
+            return True
+
+        else:
+            return False
+    except Exception:
         return False
 
 # Coded by EnGentech
