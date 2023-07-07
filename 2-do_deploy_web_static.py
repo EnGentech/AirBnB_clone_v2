1a2333bbb171a7d84cd0173b189d94fa46b86102
EnGentech <engen.inyang@gmail.com>
2023-07-07 09:08:58 -0700

    deployment

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Automated deployment"""
+
+from fabric.api import *
+from os import path
+
+env.hosts = ['ubuntu@52.91.127.148', 'ubuntu@34.207.58.138']
+current_time = datetime.now()
+tim = current_time.strftime('%Y%m%d%H%M%S')
+
+def do_deploy(archive_path):
+    """Function for web deployment"""
+    if path.exits('archive_path'):
+        put(archive_path, '/tmp/')
+
+        commands = [
+        "mkdir -p /data/web_static/releases/web_static_{}".format(tim)
+        "tar -xzf /data/web_static/releases/web_static_{}".format(tim)
+        "rm /tmp/web_static_".format(tim)
+        "mv /data/web_static/release/web_static_{}/\
+        web_static/* /data/web_static/releases/\
+        web_static_{}".format(tim, tim)
+        "rm -rf /data/web_static/releases/\
+        web_static_{}/web_static".format(tim)
+        "rm -rf /data/web_static/current"
+        "ln -s /data/web_static/releases/web_static_{}\
+         /data/web_static/current".format(tim)
+        
+    else:
+        return False
