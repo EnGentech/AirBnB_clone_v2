90afd2fa718201a41588cc5f78a410f178d6c213
EnGentech <engen.inyang@gmail.com>
2023-07-07 13:00:01 -0700

    modified

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,36 +1,33 @@
-#!/usr/bin/python3
+#!/usr/bin/python5
 """Automated deployment"""
 
 
 from fabric.api import *
-from datetime import datetime
 from os import path
 
 env.hosts = ['52.91.127.148', '34.207.58.138']
 env.user = 'ubuntu'
 
-current_time = datetime.now()
-tim = current_time.strftime('%Y%m%d%H%M%S')
-
 
 def do_deploy(archive_path):
     """Function for web deployment"""
     if path.exists(archive_path):
+        tim = archive_path[20:-4]
         put(archive_path, '/tmp/')
-        
-        commands = ["mkdir -p /data/web_static/releases/\
-        web_static_{}".format(tim), "tar -xzf /data/web_static/\
-        releases/web_static_{}".format(tim), "rm /tmp/\
-        web_static_{}".format(tim), "mv /data/web_static/release/\
-        web_static_{}/web_static/* /data/web_static/releases/\
-        web_static_{}".format(tim, tim), "rm -rf /data/\
-        web_static/releases/web_static_{}/\
-        web_static".format(tim), "rm -rf /data/web_static/\
-        current", "ln -s /data/web_static/releases/\
-        web_static_{}/data/web_static/current".format(tim)]
 
-        for x in commands:
-            run(x)
+        pad = "/data/web_static/releases/web_static"
+
+        a = "mkdir -p {}{}".format(pad, tim)
+        b = "tar -xzf /tmp/web_static_{}.tgz -C {}{}".format(tim, pad, tim)
+        c = "rm /tmp/web_static_{}.tgz".format(tim)
+        d = "mv {}{}/web_static/* {}{}".format(pad, tim, pad, tim)
+        e = "rm -rf {}{}/web_static".format(pad, tim)
+        f = "rm -rf /data/web_static/current"
+        g = "ln -s {}{}/data/web_static/current".format(pad, tim)
+        
+        commands = [a, b, c, d, e, f, g]
+        for command in commands:
+            run(command)
 
     else:
         return False
