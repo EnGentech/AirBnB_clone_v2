99c1b794a4dad3771af901c3f96977462ea727db
EnGentech <engen.inyang@gmail.com>
2023-07-23 02:21:57 -0700

    render list of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 """Python script for Airbnb flask"""
 
-from flask import Flask
+from flask import Flask, render_template
 from models import storage
+
 
 app = Flask(__name__)
 
 
+@app.route("/states_list", strict_slashes=False)
+def all_states():
+    list_states = storage.all("State").values()
+    return render_template("7-states_list.html", list_states=list_states)
+
+
+@app.teardown_appcontext
+def close_session():
+    storage.close()
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
+    
+# Coded by EnGentech
